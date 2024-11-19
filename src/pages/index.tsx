d5cac7a2a0b4b036e2bc17bc9ecc01b2291b72de
fatihmuhamadridho <fatih.ridho@salt.co.id>
2024-11-20 00:25:47 +0700

    feat: 🎸 update content homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,122 +1,128 @@
 import { Hero, Layout } from "@/components";
-import { Box } from "@mantine/core";
+import {
+  BackgroundImage,
+  Badge,
+  Box,
+  Button,
+  Center,
+  Flex,
+  List,
+  Stack,
+  Text,
+} from "@mantine/core";
+import { IconHeartRateMonitor } from "@tabler/icons-react";
+import Image from "next/image";
 import React from "react";
 
 const HomePage = () => {
   return (
     <Layout>
       <Hero />
-      <Box bg={"#1778ff"} h={288} w={"100%"}></Box>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim beatae
-        exercitationem, iusto itaque nobis tempore. Dolores iste ipsa sed
-        temporibus quam possimus id laboriosam perferendis placeat quod quidem
-        mollitia soluta beatae officiis consectetur, nostrum necessitatibus
-        autem voluptas eum aperiam voluptates. Iure suscipit sit quae maiores
-        voluptas autem culpa inventore, distinctio placeat necessitatibus
-        incidunt omnis atque tenetur! Corporis earum quibusdam ullam cumque,
-        adipisci accusantium consectetur! Tempora tenetur id placeat amet, nulla
-        iste recusandae quidem eligendi cupiditate natus reiciendis quibusdam
-        aliquam corrupti similique dolor voluptatum animi et, voluptatem,
-        necessitatibus incidunt tempore quasi repudiandae illum vero? Quas ex
-        ea, laborum omnis iusto assumenda iste quisquam, dolore facilis impedit
-        officia, voluptatem dolorum debitis quo saepe corrupti! Aspernatur quo
-        eius, quasi maxime optio nisi? Voluptates quia autem quas corporis?
-        Ducimus obcaecati incidunt odio cum hic! A voluptate aliquid officia
-        cum? Expedita id quaerat hic suscipit assumenda necessitatibus incidunt
-        reiciendis adipisci! Dolorum explicabo distinctio consequuntur maiores.
-        Deserunt, dignissimos recusandae quae placeat culpa debitis sapiente,
-        officia molestias rem ea sequi vero voluptas harum. Ea, ducimus. Animi
-        vitae repudiandae, dolores, nesciunt sint nisi voluptates repellat dolor
-        tempore recusandae consectetur saepe reiciendis eum temporibus et quam,
-        quod cum nam minus. Culpa, laborum accusantium? Id rerum eligendi quas
-        maxime voluptatibus, earum ducimus dolorum recusandae sint architecto
-        dolores adipisci officia suscipit, omnis facere et eius cumque quis unde
-        impedit, enim hic. Deserunt dolore hic, dolor necessitatibus unde
-        tempora ipsum libero quaerat illum aliquid autem quod perspiciatis nam,
-        sapiente ad consequatur at distinctio, mollitia sit sequi cupiditate.
-        Unde quia, maiores aliquam quis aspernatur totam molestiae placeat qui,
-        amet distinctio quam. Repudiandae commodi explicabo laudantium
-        reiciendis dolores accusamus suscipit voluptatibus, dolorum atque beatae
-        ipsum et dicta unde quisquam neque a, doloribus harum nemo enim ex
-        sequi? Optio repellendus ratione quam explicabo beatae eaque, repellat
-        nostrum dolore ea ducimus. Deleniti impedit porro aliquid! Impedit
-        molestiae nulla consequatur neque aut blanditiis delectus fugiat
-        voluptatum repudiandae, consectetur numquam alias. Veritatis nesciunt
-        dolorum vel soluta ut unde perspiciatis nemo iusto pariatur qui
-        voluptates adipisci distinctio quisquam mollitia natus obcaecati libero
-        aliquid ipsum quasi debitis, corporis cupiditate delectus minus! Quos
-        alias quaerat possimus praesentium beatae soluta distinctio similique
-        iusto est iste quasi quod amet accusamus vero, hic dolor quibusdam qui
-        nemo impedit ad laudantium nisi dolorum expedita? Eligendi quaerat rem
-        in voluptas ratione omnis iste quod dolores suscipit debitis natus quis
-        facere, amet qui dolore culpa sint quas veritatis accusantium dolorum
-        autem perferendis fugit laudantium accusamus. Assumenda voluptatem
-        deleniti optio, eveniet alias laborum officia ut, nobis temporibus ab
-        explicabo accusamus reiciendis quisquam quo enim nisi facilis animi
-        odio! Consequatur vel facilis enim non ad id quas optio, natus provident
-        quam quis quod magnam maiores minus libero explicabo laudantium
-        consectetur at quia dolore laborum, molestias, ab quos? Ab iure commodi
-        odio a. Error ducimus modi veritatis dolorum soluta, ratione rem
-        repudiandae dignissimos, voluptatibus harum, optio nemo ab perferendis
-        dolorem! At velit iusto eos molestias alias? Numquam doloremque,
-        laboriosam reiciendis esse obcaecati qui accusantium, doloribus aut,
-        quaerat quis tenetur fugit architecto! Molestiae inventore earum
-        laboriosam laudantium necessitatibus eos facere itaque amet minima,
-        tempora, ipsam neque ullam hic libero? Quisquam veniam, ad hic quo
-        expedita officia quasi iste pariatur vel quis corrupti eius, mollitia
-        incidunt nisi necessitatibus magni beatae sunt odio suscipit dolores
-        illo nulla, temporibus eos. Veritatis esse exercitationem sunt facilis
-        aperiam quos excepturi totam aspernatur, debitis, qui reiciendis nostrum
-        iste et quaerat delectus hic dicta ex voluptas deserunt cum consequatur
-        nihil id cumque sapiente. Nemo, aliquam! Fuga omnis quas sequi velit
-        sapiente aperiam dolor cumque laudantium, ipsam veniam iste labore
-        nostrum sint fugit mollitia beatae officia deserunt consequatur
-        corrupti. Cupiditate tempora quam, repudiandae molestias eos nemo maxime
-        deleniti dicta nobis commodi deserunt voluptates quis saepe esse rem
-        aperiam blanditiis laboriosam nihil eum itaque? Blanditiis, minima.
-        Molestiae ea atque omnis aliquam exercitationem sunt, doloribus illum
-        odio eum id! Tempore assumenda quas, animi cum dicta ducimus asperiores?
-        Quod aperiam accusantium optio. Sit, atque facere numquam deserunt
-        voluptas minus eos sapiente laudantium sequi officia a quaerat commodi
-        omnis! Minima nulla sit ea? Saepe optio odio libero obcaecati ipsam
-        doloribus sunt dolore reiciendis error dolorum illum vel repellat
-        possimus eos cumque, ea totam sit. Tempore eos, nihil debitis ab
-        voluptates, consequuntur ex magni quod animi incidunt fuga dolore nam.
-        Doloremque quod inventore sit delectus fugiat dolorum magni, non a illum
-        culpa. Atque, voluptatibus? Fuga sunt quidem quam unde culpa,
-        accusantium quo numquam blanditiis odit non ab, possimus mollitia illum
-        eius. Itaque deleniti aspernatur vitae voluptatibus eligendi mollitia
-        delectus deserunt? Quaerat delectus autem sit quidem cupiditate suscipit
-        eaque omnis. Repellendus tenetur dolorem ipsa ducimus vel magnam,
-        dolorum asperiores accusantium nihil atque. Magni mollitia cumque iure
-        numquam in suscipit aliquid iste quas, beatae accusantium cum placeat
-        nam, qui labore, voluptates molestiae velit. Vero provident iure
-        consequatur veritatis neque, placeat maiores eius cumque ab, officiis
-        deleniti voluptas reprehenderit ea. Numquam ducimus quisquam delectus
-        aspernatur distinctio eaque obcaecati molestiae labore. Ex officia iste
-        voluptatem, accusamus facere dicta! Magnam itaque velit unde officia
-        ratione sed dignissimos reprehenderit delectus corrupti magni ullam est
-        corporis maxime minus porro illum, blanditiis quam voluptates mollitia
-        nostrum aut? Tempore unde, rem illo voluptatum repudiandae ab voluptas
-        magni velit eius optio, provident impedit. Iste doloribus nesciunt
-        reprehenderit quam adipisci! Nobis cupiditate animi molestiae doloribus,
-        iste ipsam! Perspiciatis rerum delectus, assumenda doloremque reiciendis
-        corporis sequi eveniet quo nesciunt tempora facilis beatae modi. Nam,
-        cumque modi laudantium quo, doloribus ipsam iste sed corporis quod,
-        laboriosam quam quisquam. Deleniti assumenda amet quo sit placeat velit
-        voluptas eum nobis ullam quidem natus explicabo rerum, maiores eius ea
-        inventore repellendus dignissimos quas. Cum beatae voluptates quam illum
-        quasi placeat assumenda harum voluptatem dolorum omnis animi quibusdam
-        eveniet accusamus tempora ex numquam maiores, porro suscipit ullam!
-        Labore sapiente saepe deleniti odio ea officiis exercitationem. Aliquam
-        mollitia distinctio atque quas quibusdam facilis ab officiis nisi magnam
-        optio illo iure sit repudiandae alias odio unde impedit est, aspernatur
-        sapiente perspiciatis, pariatur non? Sequi commodi, blanditiis, suscipit
-        iusto sed officia consequuntur esse laborum molestiae cum nihil nam
-        impedit incidunt doloremque pariatur. Debitis placeat magni aperiam
-        aliquam, consequuntur officia iure. Autem, itaque! Sequi, est?
-      </div>
+      <Center bg={"#1778ff"} h={288} w={"100%"} p={20}>
+        <Flex direction={"column"} maw={1110} w={"100%"} gap={64}>
+          <Flex>
+            <Text>cs-jkt@legion-indo.com</Text>
+            <Text>(021) 29456666</Text>
+            <Flex direction={"column"} align={"center"}>
+              <Text>Working Hours :</Text>
+              <Text>Mon - Fri: 8.30AM to 5PM,</Text>
+              <Text>Sat : 8.30AM to 1.30PM</Text>
+            </Flex>
+          </Flex>
+          <Flex>
+            <Box maw={730}>
+              <Text>About Us</Text>
+              <Text>
+                At Legion Logistik Indonesia, we pride ourselves on our capacity
+                to give benefit to our clients from our extensive experience and
+                knowledge in creating logistics solutions to suits client needs.
+                Utilizing our worldwide connections, we can streamline logistics
+                barriers and building a strong supply chain.
+              </Text>
+            </Box>
+            <Button>Read More</Button>
+          </Flex>
+        </Flex>
+      </Center>
+      <Center w={"100%"} mt={60}>
+        <Flex maw={1110}>
+          <Image
+            src={"/images/world_map.webp"}
+            alt="world_map"
+            width={635}
+            height={314}
+          />
+          <Box>
+            <Text>Global Partner</Text>
+            <List className="!list-decimal">
+              <List.Item>
+                <Stack gap={0}>
+                  <Text>SINGAPORE</Text>
+                  {[1, 2, 3, 4].map((item: any) => (
+                    <Text key={item}>
+                      Legion Network Group Private Limited (HQ) :
+                    </Text>
+                  ))}
+                </Stack>
+              </List.Item>
+              <List.Item>
+                <Stack gap={0}>
+                  <Text>MALAYSIA</Text>
+                  <Text>Legion Network Group Private Limited (HQ) :</Text>
+                </Stack>
+              </List.Item>
+            </List>
+          </Box>
+        </Flex>
+      </Center>
+      <Center mt={60} mb={30}>
+        <Flex direction={"column"} align={"center"}>
+          <Badge>SERVICES</Badge>
+          <Text mt={24} fz={32}>
+            What Can We Do For You ?
+          </Text>
+          <Flex mt={50} gap={30}>
+            {[1, 2, 3].map((item) => (
+              <Flex key={item} direction={"column"} maw={286}>
+                <Flex>
+                  <IconHeartRateMonitor size={50} />
+                  <Text>Supply Chain Management</Text>
+                </Flex>
+                <Text>
+                  We help our clients to create and manage high-performing
+                  supply chains that drive sustained, profitable growth.
+                </Text>
+              </Flex>
+            ))}
+          </Flex>
+        </Flex>
+      </Center>
+      <BackgroundImage
+        className="relative"
+        src="/images/warehouse2.webp"
+        h={423}
+      >
+        <Center className="bg-black/50 z-10" h={"100%"} c={"white"}>
+          <Stack maw={1110} align="center">
+            <Text ta={"center"}>PT. LEGION LOGISTIK INDONESIA</Text>
+            <Text ta={"center"}>
+              Within excess of our global network from 9400 partners in 195
+              countries creates a competitive advantage for our transportation
+              and logistics services, we are dependably at your administration.
+              We will likely give you the best worldwide, yet neighbourhood
+              benefit around. Get in touch with us today.
+            </Text>
+            <Button>Read More</Button>
+          </Stack>
+        </Center>
+      </BackgroundImage>
+      <Box py={60}></Box>
+      <Center>
+        <Flex maw={1110} direction={"column"} align={"center"}>
+          <Text>Contact</Text>
+          <Text>Get In Touch With Us</Text>
+        </Flex>
+      </Center>
     </Layout>
   );
 };
